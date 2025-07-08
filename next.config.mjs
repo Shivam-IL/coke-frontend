51798d27c9e59fa8b1915370c7a96f3407420b00
Shivam Gautam <sh1vm6@Mac.lan>
2025-07-08 12:07:06 +0530

    coke-demo

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,43 @@
+// import nextPwa from "@ducanh2912/next-pwa";
+
+// const withPWA = nextPwa({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   disable: false,
+//   devOptions: {
+//     enabled: true,
+//   },
+//   buildExcludes: [/\.map$/], // .map files excluded
+// });
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default withPWA(nextConfig);
+
+
+
 import nextPwa from "@ducanh2912/next-pwa";
 
 const withPWA = nextPwa({
@@ -14,6 +54,7 @@ const withPWA = nextPwa({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "standalone", // <-- Add this line
   images: {
     remotePatterns: [
       {
